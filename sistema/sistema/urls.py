2cf28b4e1311df7431447e5aff6081f3c4e27872
ACN-Destroyer <icefirethunderlight@gmail.com>
2024-09-25 17:51:02 -0400

    Adicionar o que falta

--- sistema/sistema/urls.py
+++ sistema/sistema/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from sistema.views import Login
+from sistema.views import Login, Logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Login.as_view(), name='Login')
+    path('', Login.as_view(), name='login'),
+    path('logout/', Logout.as_view(), name='logout'),
 ]
--- sistema/sistema/views.py
+++ sistema/sistema/views.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 class Login(View):
     """
@@ -8,4 +10,37 @@ class Login(View):
 
     def get(self, request):
         contexto = {'mensagem': ''}
-        return render(request, 'autenticacao.html', contexto)
+        if request.user.is_authenticated:
+            return redirect("/veiculo")
+        else:
+         return render(request, 'autenticacao.html', contexto)
+    
+    def post(self, request):
+
+        # obtém as credenciais de autenticação do formulário
+
+        usuario = request.POST.get('usuario', None)
+        senha = request.POST.get('senha', None)
+
+        # Verifica as credenciais de autenticação fornecidas
+        user = authenticate(request, username=usuario, password=senha)
+        if user is not None:
+
+            # Verifica se o usuario ainda está ativo no sistema
+            if user.is_active:
+                login(request, user)
+                return redirect("/veiculo")
+            
+            return render(request, 'autenticacao.html', {'mensagem': 'Usuário inativos.'})
+        
+        return render(request, 'autenticacao.html', {'mensagem': 'Usuário ou senha inválidos'})
+    
+class Logout(View):
+    """
+    Class Based View para realizar logout de usuários.
+    """
+
+    def get(self, request):
+        logout(request)
+        return redirect(settings.LOGIN_URL)
+    
